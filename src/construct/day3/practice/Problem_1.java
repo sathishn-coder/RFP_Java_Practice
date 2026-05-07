1df22b82d0f30c5af60bc0cabca795e3bbd94d52
sathishn-coder <sathish.nethaji@bridgelabz.com>
2026-05-07 15:34:28 +0530

    java construct - day 3 small changes

--- src/construct/day3/practice/Problem_1.java
+++ src/construct/day3/practice/Problem_1.java
@@ -7,65 +7,21 @@ public class Problem_1 {
 
 	// Constants
     static final int IS_PRESENT = 1;
-    static final int IS_PART_TIME = 2;
-
-    static final int WAGE_PER_HOUR = 20;
-    static final int FULL_DAY_HOURS = 8;
-    static final int PART_TIME_HOURS = 4;
-
-    static final int MAX_WORKING_DAYS = 20;
-    static final int MAX_WORKING_HOURS = 100;
+    
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Employee Wage Computation Program");
+    	System.out.println("Welcome to Employee Wage Computation");
 
-        int totalEmpHours = 0;
-        int totalWorkingDays = 0;
-        int totalSalary = 0;
-
-        List<Integer> dailyWages = new ArrayList<>();
-
-        while (totalEmpHours < MAX_WORKING_HOURS && totalWorkingDays < MAX_WORKING_DAYS) { // 0< 100 && 0 < 20
-
-            totalWorkingDays++;
-
-            int empCheck = (int) Math.floor(Math.random() * 3); // 0,1,2
-            int empHours = 0;
-
-            switch (empCheck) {
-                case IS_PRESENT:
-                    empHours = FULL_DAY_HOURS;
-                    break;
-
-                case IS_PART_TIME:
-                    empHours = PART_TIME_HOURS;
-                    break;
-
-                default:
-                    empHours = 0; // Absent
-            }
-
-            totalEmpHours += empHours;
-
-            int dailyWage = empHours * WAGE_PER_HOUR;
-            totalSalary += dailyWage;
-
-            dailyWages.add(dailyWage);
-
-            System.out.println("Day " + totalWorkingDays +
-                    " | Hours: " + empHours +
-                    " | Daily Wage: " + dailyWage);
-        }
-
-        System.out.println("\nTotal Working Days: " + totalWorkingDays);
-        System.out.println("Total Working Hours: " + totalEmpHours);
-        System.out.println("Total Salary: " + totalSalary);
-
-        System.out.println("\nDaily Wages List:");
-        for (int wage : dailyWages) {
-            System.out.print(wage + " ");
+        int isEmployeePresent = getEmployeePresence();
+        if (isEmployeePresent == IS_PRESENT) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
         }
     }
 
+    private static int getEmployeePresence() {
+        return (int) (Math.random() * 2);
+    }
 }
