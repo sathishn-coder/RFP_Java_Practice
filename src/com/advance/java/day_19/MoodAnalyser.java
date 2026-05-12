881c52df6e24adda39c6a7f3017f31c6fed6e387
sathishn-coder <sathish.nethaji@bridgelabz.com>
2026-05-12 11:49:49 +0530

    Advance java Day 19 added a Mood Analyzer

---
+++ src/com/advance/java/day_19/MoodAnalyser.java
@@ -0,0 +1,16 @@
+package com.advance.java.day_19;
+
+public class MoodAnalyser {
+
+	   public String analyseMood(String msg){
+	        if (msg.contains("SAD")){
+	            return "SAD";
+	        }else{
+	            return "HAPPY";
+	        }
+	    }
+	    public static void main(String[] args) {
+
+	    }
+
+}
