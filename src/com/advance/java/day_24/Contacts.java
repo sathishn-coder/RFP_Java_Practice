6582979074b461f11c6aafdecbff6d2d3c47504e
sathishn-coder <sathish.nethaji@bridgelabz.com>
2026-05-20 14:14:04 +0530

    Advance Java - Day 24 Address Book Problem UC 10

---
+++ src/com/advance/java/day_24/Contacts.java
@@ -0,0 +1,34 @@
+package com.advance.java.day_24;
+
+public class Contacts {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+    String email;
+
+    public Contacts(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public void showContact() {
+        System.out.println("First Name  : " + firstName);
+        System.out.println("Last  Name  : " + lastName);
+        System.out.println("Address     : " + address);
+        System.out.println("City        : " + city);
+        System.out.println("State       : " + state);
+        System.out.println("Zip         : " + zip);
+        System.out.println("Phone No    : " + phoneNumber);
+        System.out.println("Email       : " + email);
+    }
+}
