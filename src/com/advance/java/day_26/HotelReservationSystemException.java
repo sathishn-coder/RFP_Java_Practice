b667c9bd80b2cbd3f0407502279528561fcef641
sathishn-coder <sathish.nethaji@bridgelabz.com>
2026-05-20 14:26:45 +0530

    Advance Java - day 26 workshop UC11

---
+++ src/com/advance/java/day_26/HotelReservationSystemException.java
@@ -0,0 +1,7 @@
+package com.advance.java.day_26;
+
+public class HotelReservationSystemException extends Exception {
+    public HotelReservationSystemException(String message) {
+        super(message);
+    }
+}
